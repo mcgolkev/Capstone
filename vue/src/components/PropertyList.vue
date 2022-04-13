5a3604fe5f69db01d4ac63460bb0d6a3eef8c051
Maddie Meyers <maddiemeyers@outlook.com>
2022-04-13 13:04:18 -0400

    changed columns/grid/flex

--- vue/src/components/PropertyList.vue
+++ vue/src/components/PropertyList.vue
@@ -1,11 +1,28 @@
 <template>
   <div class="property-list column">
- <div class="property-information" v-for="property in this.$store.state.properties" v-bind:key="property.propertyId">
-     <!-- <img v-bind:src="property.picture" v-bind:alt="property.addres1s" /> -->
-    <h1><router-link :to="{name: 'property-info', params: {id: property.propertyId}}">Address: {{property.addressLine1}} {{property.city}} {{property.state}} {{property.zip}}</router-link></h1>
-      <p> Date Available: {{property.dateAvailable}}, ${{property.price}}</p>
-      <p>Bed: {{property.numBedrooms}} Bath: {{property.numBathrooms}} {{property.squareFeet}}sqft</p>
-      <p>Description: {{property.shortDescription}}</p>
+  <div class="tile is-ancestor">
+
+            <div class="tile is-parent">
+
+    <div class="property-information" v-for="property in this.$store.state.properties" v-bind:key="property.propertyId">
+
+                <div class="tile is-child">
+
+
+             <img v-bind:src="property.picture" />
+               </div>
+       <div class="tile is-vertical">
+            <h1><router-link :to="{name: 'property-info', params: {id: property.propertyId}}">Address: {{property.addressLine1}} {{property.city}} {{property.state}} {{property.zip}}</router-link></h1>
+
+
+
+             <p> Date Available: {{property.dateAvailable}}, ${{property.price}}</p>
+               <p>Bed: {{property.numBedrooms}} Bath: {{property.numBathrooms}} {{property.squareFeet}}sqft</p>
+          <p>Description: {{property.shortDescription}}</p>
+       </div>
+
+         </div>
+    </div>
       </div>
   </div>
 </template>
