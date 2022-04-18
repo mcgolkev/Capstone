139e39d87006d9ad5bd344da84a306895e26966e
Maddie Meyers <maddiemeyers@outlook.com>
2022-04-18 10:15:37 -0400

    nav bar

--- vue/src/components/PropertyList.vue
+++ vue/src/components/PropertyList.vue
@@ -2,27 +2,23 @@
   <div class="property-list column">
   <div class="tile is-ancestor">
 
-            <div class="tile is-parent">
+    <div class="tile is-parent">
 
     <div class="property-information" v-for="property in this.$store.state.properties" v-bind:key="property.propertyId">
 
-                <div class="tile is-child">
+    <div class="tile is-child">
 
 
-             <img v-bind:src="property.picture" />
-               </div>
+      <img v-bind:src="property.picture" />
+      </div>
        <div class="tile is-vertical">
-            <h1><router-link :to="{name: 'property-info', params: {id: property.propertyId}}">Address: {{property.addressLine1}} {{property.city}} {{property.state}} {{property.zip}}</router-link></h1>
-
-
-
-             <p> Date Available: {{property.dateAvailable}}, ${{property.price}}</p>
-               <p>Bed: {{property.numBedrooms}} Bath: {{property.numBathrooms}} {{property.squareFeet}}sqft</p>
+        <h1><router-link :to="{name: 'property-info', params: {id: property.propertyId}}">Address: {{property.addressLine1}} {{property.city}} {{property.state}} {{property.zip}}</router-link></h1>
+        <p> Date Available: {{property.dateAvailable}}, ${{property.price}}</p>
+          <p>Bed: {{property.numBedrooms}} Bath: {{property.numBathrooms}} {{property.squareFeet}}sqft</p>
           <p>Description: {{property.shortDescription}}</p>
-       </div>
-
+          </div>
          </div>
-    </div>
+       </div>
       </div>
   </div>
 </template>
