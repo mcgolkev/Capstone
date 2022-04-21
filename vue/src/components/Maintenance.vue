aaebcfcbffc4bdbf021a9c6c490d6a01b1c3753e
kevin mcgoldrick <kevin@kevinmcgoldrick.com>
2022-04-21 12:14:47 -0400

    most formatting to maintenace assigment list completed

--- vue/src/components/Maintenance.vue
+++ vue/src/components/Maintenance.vue
@@ -1,11 +1,28 @@
 <template>
-  <div>
-      <div v-for="maint in this.$store.state.maintenance" v-bind:key="maint.maintenanceId">
-          <input type="checkbox" id="maintenance-status" name="maintenance-status" :value="maint.complete" v-model="maint.complete" @change="(updateMaintenance(maint.complete, maint.maintenanceId))" />
-          <router-link :to="{name: 'maintenance', params: {id: maint.maintenanceId}}"><p>Date: {{maint.dateSubmitted}}</p>
-          <p>From: {{maint.ownershipId}}</p></router-link>
+  <div id="all">
+    <h1 id="OverallPageTitleOutsideOfTitles">
+        <center>Maintenance Assignments</center>
+    </h1>
+    <div id="this-contains-the-entire-page-and-all-contents" class="tile is-ancestor">
+        <div id="this-contains-all-tiles" class="tile is-12">
+            <div id="this-contains-all-tiles-and-makes-tiles-stack-vertically" class="tile is-parent is-vertical">
+                <div id="this-is-a-single-tile" class="tile is-12 is-child box">
+                    <div id="this-makes-all-content-within-the-tile-stack-vertically" >
+                        
+                        <div id="tileID" class="tile is-vertical box" v-for="maint in this.$store.state.maintenance" v-bind:key="maint.maintenanceId">
+                          <label for="maintenance-status">
+                            <input type="checkbox" id="maintenance-status" name="maintenance-status" :value="maint.complete" v-model="maint.complete" @change="(updateMaintenance(maint.complete, maint.maintenanceId))" />
+                            Assignment Complete</label>
+                          <router-link :to="{name: 'maintenance', params: {id: maint.maintenanceId}}"><p>Date Submitted: {{maint.dateSubmitted}}</p>
+                          </router-link>
+                        
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-    </div>
+  </div>
 </template>
 
 <script>
